8ba783944b48f55736d5a23844cff07629670e80
Ast3risk-ops <spacedude609@pm.me>
2023-10-04 19:23:06 -0400

    Add about header item

--- assets/icons/custom.js
+++ assets/icons/custom.js
@@ -1,9 +1,9 @@
-import { faBlog, faBook, faFile, faNewspaper } from '@fortawesome/free-solid-svg-icons';
+import { faBlog, faBook, faFile, faNewspaper, faUserSecret } from '@fortawesome/free-solid-svg-icons';
 // import { faAddressBook } from '@fortawesome/free-regular-svg-icons';
 // import { faAmazon, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 const icons = [
-    faBook, faBlog, faFile, faNewspaper,
+    faBook, faBlog, faFile, faNewspaper, faUserSecret,
     // faAddressBook,
     // faAmazon, faGoogle,
 ];
