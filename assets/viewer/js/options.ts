330abfb0bc9bca692f414b85d0f846660b81c0f7
Ast3risk-ops <spacedude609@pm.me>
2023-11-22 11:30:12 -0500

    Revert image viewer to default title

--- assets/viewer/js/options.ts
+++ assets/viewer/js/options.ts
@@ -1,7 +1,7 @@
 const options = {
     className: "image-viewer",
     toolbar: 2,
-    title: [2, (image, imageData) => `Image size: ${imageData.naturalWidth}px × ${imageData.naturalHeight}px`]
+    title: [2, (image, imageData) => `${image.alt} (${imageData.naturalWidth}px × ${imageData.naturalHeight}px)`]
 };
 export default options;
 
